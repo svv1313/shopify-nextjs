b0f6f75e86b76ba5444538e00d0c127f2566d89e
svv1313 <seleznev19963@gmail.com>
2024-07-23 13:02:55 +0200

    feat: basic shopify setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+    SHOPIFY_STOREFRONT_DOMAIN: process.env.SHOPIFY_STOREFRONT_DOMAIN,
+  },
+  images: {
+    domains: ['cdn.shopify.com']
+  }
+};
 
 export default nextConfig;
