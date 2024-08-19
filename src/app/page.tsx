e00724cd663f1fc750e15a2fa16120d7b12d8c04
svv1313 <seleznev19963@gmail.com>
2024-08-19 13:26:11 +0200

    feat: added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { getProductsInCollection } from '../../lib/shopify';
 import ProductList from '../../components/ProductList';
+import Hero from '../../components/Hero';
 
 export default async function Home() {
   const products = await getProductsInCollection();
 
   return (
     <section>
+      <Hero />
       <ProductList products={products} />
     </section>
   );
